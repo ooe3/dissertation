f70ab75b2912c8c87d190f728dc5d9ccf20e7a8a
Obi Emuwa <olubunmi_e@hotmail.com>
2016-06-28 16:43:30 +0100

    New code added

--- Dissertation/MainGUI.java
+++ Dissertation/MainGUI.java
@@ -3,16 +3,44 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class MainGUI implements ItemListener {
-	JPanel main;
+	JPanel main, box, login, welcome;
 	JButton button1, button2;
+	String mainPanel = "Log In";
+	String welcomePanel = "Welcome";
 	
 	public MainGUI(){
+		//default constructor
+		button1 = new JButton("Log In As Student");
+		button2 = new JButton("Log In As Administrator");
+	}
+	
+	public void addComponent(Container cn){
+		box = new JPanel();
+		String items[] = {mainPanel, welcomePanel};
+		JComboBox jb = new JComboBox(items);
+		jb.setEditable(false);
+		jb.addItemListener(this);
+		box.add(jb);
+		
+		login = new JPanel();
+		login.add(button1);
+		login.add(button2);
+		
+		cn.add(box, BorderLayout.PAGE_START);
+		cn.add(main, BorderLayout.CENTER);
 		
 	}
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 	// TODO Auto-generated method stub
+		CardLayout cl = new CardLayout();
+		main.getLayout();
+		cl.show(main, (String)e.getItem());
 	
+	}
+	
+	public void displayGUI(){
+		JFrame jf = new JFrame("University Records System");
 	}
 	
 	public void actionPerformed(ActionEvent e){
