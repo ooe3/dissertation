19858befce56b1254a782bb8f271e022c9a239c9
Obi Emuwa <olubunmi_e@hotmail.com>
2016-06-28 16:24:17 +0100

    Some added code

--- Dissertation/MainGUI.java
+++ Dissertation/MainGUI.java
@@ -2,6 +2,21 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class MainGUI {
+public class MainGUI implements ItemListener {
+	JPanel main;
+	JButton button1, button2;
+	
+	public MainGUI(){
+		
+	}
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+	// TODO Auto-generated method stub
+	
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		
+	}
 
 }
